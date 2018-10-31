b617a942ca558fa9ed4cb3f84757e25f8917906c
ahammaha <mahalakshmi@outlook.com>
2018-10-31 15:38:08 +0530

    tasks css

--- employee-management-services/app/adapters/task.js
+++ employee-management-services/app/adapters/task.js
@@ -3,11 +3,11 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
 	host: 'http://18.224.21.126:8080',
     urlForCreateRecord(modelName, snapshot) {
-    	let empId=snapshot.belongsTo('employee').id;
-        return `${this.host}/employees/${empId}/tasks`; // the url you want
-    },
-    urlForFindAll(modelName,snapshot){
-    	let empId=snapshot.adapterOptions.empId;
-    	return `${this.host}/employees/${empId}/tasks`;
-    }
+		let empId=snapshot.belongsTo('employee').id;
+		return `${this.host}/employees/${empId}/tasks`; // the url you want
+	},
+	urlForFindAll(modelName,snapshot){
+		let empId=snapshot.adapterOptions.empId;
+		return `${this.host}/employees/${empId}/tasks`;
+	}
 });
--- employee-management-services/app/routes/employee/add.js
+++ employee-management-services/app/routes/employee/add.js
@@ -1,5 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-	
+    setupController(controller){
+        controller.set('firstName', "");
+        controller.set('lastName','');
+        controller.set("email","");
+        controller.set('designation', "");
+        controller.set("empType","");
+        controller.set("eid",undefined);
+        controller.set("phoneNo",undefined);
+        controller.set("address","");
+        controller.set("dateOfBirth",undefined);
+        controller.set("langKnown","");
+	}	
 });
