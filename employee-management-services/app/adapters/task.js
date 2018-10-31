3beaa77ef710091a523b5c4c460d6674c374ae74
ahammaha <mahalakshmi@outlook.com>
2018-10-31 12:56:18 +0530

    list tasks

--- employee-management-services/app/adapters/task.js
+++ employee-management-services/app/adapters/task.js
@@ -5,5 +5,9 @@ export default DS.RESTAdapter.extend({
     urlForCreateRecord(modelName, snapshot) {
     	let empId=snapshot.belongsTo('employee').id;
         return `${this.host}/employees/${empId}/tasks`; // the url you want
+    },
+    urlForFindAll(modelName,snapshot){
+    	let empId=snapshot.adapterOptions.empId;
+    	return `${this.host}/employees/${empId}/tasks`;
     }
 });
--- employee-management-services/app/routes/employee/tasks.js
+++ employee-management-services/app/routes/employee/tasks.js
@@ -2,6 +2,10 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 	model:function(params){
-		return this.store.findAll('task',params.employee_id);
+		return this.store.findAll('task',{
+			adapterOptions: {
+				empId: params.employee_id
+			}
+		});
 	}
 });
