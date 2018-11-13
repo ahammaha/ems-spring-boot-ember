2bd767f72bec0c04dddd5325c57e6f522e638e2e
ahammaha <mahalakshmi@outlook.com>
2018-11-13 21:13:45 +0530

    just checking if its working

--- employee-management-services/app/adapters/application.js
+++ employee-management-services/app/adapters/application.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
 	//namespace: 'api',
-	host: 'http://172.31.18.94:8080'
-	//host: "http://localhost:8080"
+	//host: 'http://172.31.18.94:8080'
+	host: "http://localhost:8080"
 });
--- ems/src/main/java/com/maha/ems/task/TaskController.java
+++ ems/src/main/java/com/maha/ems/task/TaskController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.maha.ems.exception.EmployeeNotFoundException;
-
 @RestController
 @CrossOrigin
 @RequestMapping("/employees/{empid}")
