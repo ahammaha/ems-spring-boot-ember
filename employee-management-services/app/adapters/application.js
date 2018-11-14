3888cd93e80d33fb97ff3affa7b5c9b6b22313b7
ahammaha <mahalakshmi@outlook.com>
2018-11-14 10:06:16 +0530

    changing to elastic IP instead of public ip

--- employee-management-services/app/adapters/application.js
+++ employee-management-services/app/adapters/application.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
 	//namespace: 'api',
-	//host: 'http://172.31.18.94:8080'
-	host: "http://localhost:8080"
+	host: 'http://3.16.101.116:8080'
+	//host: "http://localhost:8080"
 });
